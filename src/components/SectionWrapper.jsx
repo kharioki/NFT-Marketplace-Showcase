b804ce31a9dbd76d4ffa7397078885f43fde2ce3
kharioki <tony.kharioki@gmail.com>
2022-11-05 14:24:40 +0300

    section wrapper

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -2,13 +2,23 @@ import styles from '../styles/Global'
 import assets from '../assets'
 import Button from './Button'
 
-const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
+const SectionWrapper = ({ title, description, showBtn, mockupImg, banner, reverse }) => {
   return (
-    <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
-      <div className={`flex items-center ${styles.boxClass}`}>
-        <div className={`${styles.descDiv} `}>
-          <h2 className={`${styles.h1Text}`}>{title}</h2>
-          <p className={`${styles.descriptionText}`}>{description}</p>
+    <div className={
+      `min-h-screen 
+      ${styles.section} 
+      ${reverse ? styles.bgWhite : styles.bgPrimary}
+      ${banner}`
+    }>
+      <div className={
+        `flex items-center 
+        ${reverse ? styles.boxReverseClass : styles.boxClass}
+        ${reverse ? styles.textRight : styles.textLeft}
+        w-11/12 sm:w-full minmd:w-3/4`
+      }>
+        <div className={`${styles.descDiv} ${reverse ? 'fadeRightMini' : 'fadeLeftMini'}`}>
+          <h1 className={`${reverse ? styles.blackText : styles.whiteText} ${styles.h1Text}`}>{title}</h1>
+          <p className={`${reverse ? styles.blackText : styles.whiteText} ${styles.descriptionText}`}>{description}</p>
           {showBtn && (
             <Button
               assetUrl={assets.expo}
@@ -16,9 +26,17 @@ const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
             />
           )}
         </div>
-      </div>
 
-      {/* <img src={mockupImg} alt="mockup" /> */}
+        <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
+          <img 
+            src={mockupImg} 
+            alt="mockup" 
+            className={`
+              ${reverse ? 'fadeLeftMini' : 'fadeRightMini'}
+              ${styles.sectionImg}
+            `} />
+        </div>
+      </div>
     </div>
   )
 }
